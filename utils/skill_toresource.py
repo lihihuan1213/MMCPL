0091d81f1091f5092bfb980a2c5b368daa6f7319
lihuihuan1213 <2271013505@qq.com>
2026-01-23 13:59:20 +0800

    commit

--- utils/skill_toresource.py
+++ utils/skill_toresource.py
@@ -7,5 +7,5 @@ def preprocess_skill_to_resource(csv_path):
     for skill_id, group in df.groupby('knowledge_concept_id'):
         resource_ids = list(set(group['resource_id'].tolist()))
         skill_to_resources[int(skill_id)] = resource_ids
-    return skill_to_resources
+    return skill_to_resources 
 
